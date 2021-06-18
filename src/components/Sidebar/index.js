8b9c4d5e2d0e5ea4f242314e6da0455c5ef848a0
Bruno Valles <49284760+BrunoValles@users.noreply.github.com>
2021-06-18 18:19:43 -0500

    initialized sidebar component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SidebarContainer, Icon, ColseIcon} from './SidebarElements';
+
+const Sidebar = ( ) => {
+    return(
+      <SidebarContainer>
+	<Icon>
+	  <CloseIcon />
+	</Icon>
+      </SidebarContainer>
+    );
+}
+
+export default Sidebar;
