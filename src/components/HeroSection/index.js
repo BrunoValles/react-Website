139a1fe9723c98cbf45ec21f340c314679416e31
Bruno Valles <49284760+BrunoValles@users.noreply.github.com>
2021-06-19 16:23:41 -0500

    initialized HeroContainer and prepared things for a video to be added

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Video from '../../videos/videos.mp4'
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg
+} from './HeroElements';
 
 const HeroSection = () => {
-    return{
+    return(
 	<HeroContainer>
 	  <HeroBg>
+	    <VideoBg autoPlay loop muted src = {Video} type='video/mp4' />
 	  </HeroBg>
 	</HeroContainer> 
-    }
+    )
 }
+
+export default HeroSection;
