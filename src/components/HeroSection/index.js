4a52ff9a823bed4c5da86f08ff2d9611330bd8cc
Bruno Valles <49284760+BrunoValles@users.noreply.github.com>
2021-07-01 21:04:09 -0500

    Changed the Herosection text

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -16,7 +16,7 @@ const HeroSection = () => {
 	    <VideoBg autoPlay loop muted src = {Video} type='video/mp4' />
 	  </HeroBg>
 	  <HeroContent>
-	    <HeroH1>Bruno Valles</HeroH1>
+	    <HeroH1>Hello, I'm Bruno Valles</HeroH1>
 	    <HeroP> >I Code and Stuff</HeroP>
 	  </HeroContent>
 	</HeroContainer> 
